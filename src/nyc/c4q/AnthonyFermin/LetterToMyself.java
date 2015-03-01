59af25fea807bd48ffe3b51b7ee173c375f635c3
AnthonyFermin <AnthonyFerminAFC@gmail.com>
2015-03-01 17:41:00 -0500

    initial commit

---
+++ src/nyc/c4q/AnthonyFermin/LetterToMyself.java
@@ -0,0 +1,21 @@
+package nyc.c4q.AnthonyFermin;
+
+/**
+ * Created by c4q-anthonyf on 3/1/15.
+ */
+public class LetterToMyself {
+
+    public static void main(String[] args){
+
+        System.out.println( "+----------------------------------------------------+" );
+        System.out.println( "|                                              ####  |" );
+        System.out.println( "|  Anthony Fermin                              ####  |" );
+        System.out.println( "|                                              ####  |" );
+        System.out.println( "|                    Future Anthony Fermin           |" );
+        System.out.println( "|                                                    |" );
+        System.out.println( "|                                                    |" );
+        System.out.println( "+----------------------------------------------------+" );
+
+    }
+
+}
---
+++ src/nyc/c4q/AnthonyFermin/Main.java
@@ -0,0 +1,21 @@
+package nyc.c4q.AnthonyFermin;
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello Again" );
+        System.out.println( "I like typing this." );
+        System.out.println( "This is fun." );
+        System.out.println( "Yay! Printing." );
+        System.out.println( "I'd much rather you 'not'." );
+        System.out.println( "I \"said\" do not touch this." );
+
+        System.out.println("Row row row your boat\n\n\ngently down the stream." );
+
+        System.out.println( "\n\nSystem.out.println( \"Hello World!\" );" );
+
+
+    }
+}
