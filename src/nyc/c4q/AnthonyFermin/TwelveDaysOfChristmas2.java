f3068625b55fa1728fa4f33cdae27ade88e3d08d
AnthonyFermin <AnthonyFerminAFC@gmail.com>
2015-03-04 02:17:32 -0500

    added comments to TwelveDaysOfChristmas2.java for clarity

--- src/nyc/c4q/AnthonyFermin/TwelveDaysOfChristmas2.java
+++ src/nyc/c4q/AnthonyFermin/TwelveDaysOfChristmas2.java
@@ -2,18 +2,20 @@ package nyc.c4q.AnthonyFermin;
 
 /**
  * Created by c4q-anthonyf on 3/4/15.
+ * Twelve Days of Christmas Lyrics printed into console
  */
 public class TwelveDaysOfChristmas2 {
 
     public static void main (String[] args){
 
+        //Strings for parts of the song that is repeated
         String onThe = "On the ";
         String dayOf = " day of Christmas my true love sent to me ";
         String and = "\nand ";
         String comma = ", \n";
 
+        //created an array with all twelve gifts from the song
         String gift[] = new String[12];
-
         gift[0] = "a Partridge in a Pear Tree.";
         gift[1] = "Two Turtle Doves";
         gift[2] = "Three French Hens";
@@ -27,8 +29,8 @@ public class TwelveDaysOfChristmas2 {
         gift[10] = "Eleven Pipers Piping";
         gift[11] = "Twelve Drummers Drumming";
 
+        //created an array for First through Twelfth
         String day[] = new String[12];
-
         day[0] = "First";
         day[1] = "Second";
         day[2] = "Third";
@@ -42,11 +44,17 @@ public class TwelveDaysOfChristmas2 {
         day[10] = "Eleventh";
         day[11] = "Twelfth";
 
+        //Main loop iterates through the ith day of the song
         for(int i = 0; i < 12; i++){
+            //prints first line of song
             System.out.println(onThe + day[i] + dayOf);
 
+            //nested loop iterates from the ith gift to the first gift
             for(int j = i; j > -1; j--){
+                //prints gift line
                 System.out.print(gift[j]);
+
+                //conditional that determines whether to put a comma, an and or nothing at all
                 if(j > 1){
                     System.out.print(comma);
                 }else if(j == 1){
@@ -54,6 +62,7 @@ public class TwelveDaysOfChristmas2 {
                 }
             }
 
+            //ends a line and skips another
             System.out.println();
             System.out.println();
 
